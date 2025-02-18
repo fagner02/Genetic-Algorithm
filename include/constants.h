4bd6e3420ef0bc475a96e8c5509447d9f1531c36
fagner ferreira <fagnerrc12@gmail.com>
2025-02-18 12:13:54 -0300

    refactor: move D_EPSILON constant to hsl.h and remove from constants.h

--- include/constants.h
+++ include/constants.h
@@ -2,8 +2,6 @@
 
 #pragma once
 
-const double D_EPSILON = 0.00000000000001;
-
 const point dirs[] = {
     /*0: left*/ {-1, 0},
     /*1: right*/{1, 0},
--- include/ui/hsl.h
+++ include/ui/hsl.h
@@ -5,6 +5,8 @@
 
 #pragma once
 
+const double D_EPSILON = 0.00000000000001;
+
 struct HSL {
     double Hue;
     double Saturation;
